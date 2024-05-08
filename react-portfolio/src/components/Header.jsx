439515ee65892bd2775ab735979575852bda9a0c
Youthfulmaster <Dennisangelov3@gmail.com>
2024-05-08 10:00:34 -0700

    Added header css

--- react-portfolio/src/components/Header.jsx
+++ react-portfolio/src/components/Header.jsx
@@ -1,12 +1,12 @@
-// src/components/Header.jsx
+
 import Navigation from './Navigation';
-import './Header.css'; // Make sure this CSS file exists
+import './Header.css'; 
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header-content">
-        <h1 className="header-title">Dennis Angelov</h1> 
+        <h1 className="header-title">Dennis Angelov</h1>
         <Navigation />
       </div>
     </header>
