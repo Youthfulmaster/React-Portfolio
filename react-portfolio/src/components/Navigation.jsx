eb5813cdfe55f02df5c9a860c6514d36adedc9dd
Youthfulmaster <Dennisangelov3@gmail.com>
2024-05-09 06:32:41 -0700

    Updated navigation to fix routes and updated index.css to have navigation align to the top and not the right

--- react-portfolio/src/components/Navigation.jsx
+++ react-portfolio/src/components/Navigation.jsx
@@ -4,10 +4,10 @@ import './Navigation.css';
 const Navigation = () => {
   return (
     <nav>
-      <a href="#about">About Me</a>
-      <a href="#portfolio">Portfolio</a>
-      <a href="#contact">Contact</a>
-      <a href="#resume">Resume</a>
+      <a href="/about">About Me</a>
+      <a href="/portfolio">Portfolio</a>
+      <a href="/contact">Contact</a>
+      <a href="/resume">Resume</a>
     </nav>
   );
 };
